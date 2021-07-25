e8939035fd36212a20b3acc7c57ecbadac99aa54
Wladut <vrinceanu.ioan.vladut@gmail.com>
2021-07-26 00:19:02 +0300

    LD-21 #comment added local project first commit

---
+++ src/com/notes/ReadFolders.java
@@ -0,0 +1,62 @@
+package com.notes;
+
+import javafx.scene.control.TreeItem;
+import java.io.File;
+
+/**
+ * Created by Vrinceanu Vladut 01-04-2021
+ * Time 20:03
+ */
+
+class ReadFolders {
+
+    static TreeItem<String> root = new TreeItem<>("Root");
+
+    static void clear(){
+        root = new TreeItem<>("Root");
+    }
+
+     private static TreeItem<String> addTreeItemToRoot(String item, TreeItem<String> root) {
+         TreeItem<String> actualItem = new TreeItem<>(item);
+         root.getChildren().add(actualItem);
+         return actualItem;
+    }
+
+
+      static void getStructure(String folderPath, TreeItem<String> rootItem) {
+
+        File actualFolderPath = new File(folderPath);
+        File[] getFirstFolders = actualFolderPath.listFiles();
+
+        assert getFirstFolders != null;
+        try {
+            for (File folder : getFirstFolders) {
+                if(folder.getName().contains(".txt") || checkIfTxtExistsInFolder(folder)){
+                    TreeItem<String> actualItem = addTreeItemToRoot(folder.getName(), rootItem);
+                    getStructure(folder.getAbsolutePath(), actualItem);}
+            }
+        } catch (NullPointerException e) {
+            //System.out.println("From ReadFolders.getStructure(): " + e.getMessage());
+        }
+    }
+
+    private static boolean checkIfTxtExistsInFolder (File folder){
+        if(folder.getName().contains(".txt")){
+            return true;
+        }
+        File[] getFirstFolders = folder.listFiles();
+        try{
+            assert getFirstFolders != null;
+            for (File file : getFirstFolders){
+                if (file.getName().contains(".txt")){
+                    return true;
+                } else if (file.isDirectory()){
+                    if (checkIfTxtExistsInFolder(file)) return true;
+                }
+            }
+        } catch (NullPointerException e) {
+            //System.out.println(e.getMessage());
+        }
+        return  false;
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Vrinceanu Vladut
+ * Date: 25-07-2021
+ * Time: 23:42
+ * OS: Windows
+ */
+
+module Notes {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.commons.io;
+
+    opens com.notes;
+}
